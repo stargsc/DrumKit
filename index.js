53a59fe68bdb754dc6cb314ee8289e86671bb70a
Gandhe Sai Chandar <153263506+stargsc@users.noreply.github.com>
2023-12-08 14:35:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// Detecting Button Press
+
+var i = 0;
+while (i < 7) {
+  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+  i++;
+}
+
+function handleClick() {
+  var buttonInnerHTML = this.innerHTML;
+  makesound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+}
+// Detecting Keyboad Press
+document.addEventListener("keypress", function(event) {
+  makesound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
